bcff33068684c9418586ac77df603c7065a2b558
root <root@vm-cluster-node2>
2014-12-15 21:41:00 +0000

    small bug fixes to solr recipe

--- solr_recipe/build.sh
+++ solr_recipe/build.sh
@@ -8,22 +8,19 @@ else
     export JRE_HOME="/usr/lib/jvm/jre"
 fi
 
-mkdir -vp ${PREFIX}/solr_pkg;
-mkdir -vp ${PREFIX}/bin;
-
-
-# cd solr-4.10.2/example/solr/collection1/conf
-# mv schema.xml schema.xml.org
-
-# cp ~/anaconda/envs/memex/conf/schema.xml .
-# sed 's/^\(.*EnglishPorterFilterFactory.*\)/<!--\1-->/g' schema.xml > tmp
-# sed 's/^\(.*protwords.*\)/<!--\1-->/g' tmp > schema.xml
-
-cp -r bin/ ${PREFIX}/bin
-
 SOLR_INSTALL=${PREFIX}/solr_pkg
-cp -r example/ ${SOLR_INSTALL}
+
+mkdir -vp ${PREFIX}/bin;
+mkdir -vp ${SOLR_INSTALL};
+
+cp -r bin/* ${PREFIX}/bin
+cp -r example/* ${SOLR_INSTALL}
+ls ${SOLR_CONF}
+
 SOLR_CONF=${SOLR_INSTALL}/solr/collection1/conf
 
 mv ${SOLR_CONF}/schema.xml ${SOLR_CONF}/schema.xml.org
 cp ${RECIPE_DIR}/schema.xml ${PREFIX}/solr_pkg/solr/collection1/conf/
+
+#adding log dir
+mkdir -vp ${SOLR_INSTALL}/logs
