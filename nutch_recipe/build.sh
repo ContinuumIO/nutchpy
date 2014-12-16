f82fce570b2c7a4cc282043790ff3366585432fc
root <root@vm-cluster-node2>
2014-12-16 01:22:41 +0000

    making copies explicit build.sh

--- nutch_recipe/build.sh
+++ nutch_recipe/build.sh
@@ -7,6 +7,7 @@ else
     export JAVA_HOME="/usr/lib/jvm/java"
     export JRE_HOME="/usr/lib/jvm/jre"
 fi
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle-cloudera/
 
 mkdir -vp ${PREFIX}/bin;
 mkdir -vp ${PREFIX}/lib;
@@ -17,9 +18,9 @@ mkdir -vp ${PREFIX}/conf;
 ant
 
 pushd runtime/local/
-cp -r bin/ ${PREFIX}/bin
-cp -r lib/ ${PREFIX}/lib
-cp -r plugins/ ${PREFIX}/plugins
-cp -r conf/ ${PREFIX}/conf
+cp -r bin/* ${PREFIX}/bin/
+cp -r lib/* ${PREFIX}/lib/
+cp -r plugins/* ${PREFIX}/plugins/
+cp -r conf/* ${PREFIX}/conf/
 
 cp ${RECIPE_DIR}/nutch-site.xml ${PREFIX}/conf/
